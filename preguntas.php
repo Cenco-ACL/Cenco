6ab6950c8e3f125ef87f5d08c4399fbd8ae54fa7
Luis Cordova <91169088+LuchitoCordova@users.noreply.github.com>
2022-08-17 11:35:23 -0400

    commit

--- preguntas.php
+++ preguntas.php
@@ -19,55 +19,72 @@
     <div class="mx-auto">
       <div class="row">
         <div class="col-md-3"></div>
-        <div class="col-md-6">
-          <div class="progress">
-            <br><br>
-            <div class="progress-bar w-25" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-          </div><br>
+        <div class="col-md-1">
+          <img src="img/1.png" alt="" width="100" height="100">
         </div>
+        <div class="col-md-1">
+          <br>
+          <div class="progress">
+            <div class="progress-bar w-25" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          </div>
+          <div class="col-md-1">
+            <img src="img/2.png" alt="" width="100" height="100">
+          </div>
+          <div class="col-md-1">
+            <br>
+            <div class="progress">
+              <div class="progress-bar w-25" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <img src="img/3.png" alt="" width="100" height="100">
+          </div>
+          <div class="col-md-1"></div>
         <div class="col-md-3"></div>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Pregunta 1</h5>
-                  <p class="card-text">¿Cuál es la capital de España?</p>
-                  <form action="">
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                      <label class="form-check-label" for="exampleRadios1">
-                      Ejemplo 1
+
+      </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Pregunta 1</h5>
+                <p class="card-text">¿Cuál es la capital de España?</p>
+                <form action="">
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                    Ejemplo 1
+                  </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                    Ejemplo 2
                     </label>
-                    </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                      Ejemplo 2
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                      Ejemplo 3
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                      Ejemplo  4
-                      </label>
-                    </div>
-                  <a href="preguntas.php" class="btn btn-primary">Siguiente</a>
-                  </form>
-                </div>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                    Ejemplo 3
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                    Ejemplo  4
+                    </label>
+                  </div>
+                <a href="preguntas.php" class="btn btn-primary">Siguiente</a>
+                </form>
               </div>
             </div>
-            <div class="col-md-3"></div>
           </div>
+          <div class="col-md-3"></div>
         </div>
       </div>
-    </div>
   </body>
 </html>
